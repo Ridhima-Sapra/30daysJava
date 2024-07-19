9ad3fc1f6e18f68597ce109f7963a02ab578cbc7
Ridhima-Sapra <ridhimasapra10@gmail.com>
2024-07-19 16:36:58 +0530

    day1 file added

---
+++ day1.js
@@ -0,0 +1,22 @@
+var a=67;
+var b="ridhima";
+console.log(a);
+console.log(b);
+const c=true;
+console.log(c);
+str="i am a student";
+var_bool=false;
+var_num=78;
+let obj={Name:"kavita" , Relation: "mother"};
+let array=[54,87,43,76];
+console.log(typeof(str));
+console.log(typeof(var_bool));
+console.log(typeof(var_num));
+console.log(typeof(obj));
+console.log(typeof(array));
+{
+    let abc=78;
+    console.log(abc);
+}
+abc=899;
+console.log(abc);
