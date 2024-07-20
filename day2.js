aa5dabf87a3b1de3b3e5cba9d644ebba43e97f4d
Ridhima-Sapra <ridhimasapra10@gmail.com>
2024-07-20 19:58:59 +0530

    day2 file added

---
+++ day2.js
@@ -0,0 +1,115 @@
+//activity 1 : ARITHEMATIC OPERATOR
+
+//task 1 : addition of two numbers
+var1=8
+var2=6
+result=var1+var2
+console.log("the sum is ", result);
+ // output : sum of 8 and 6 is 14
+
+//task2 : subtraction of two numbers
+ 
+// output :subtraction of 12 and 5 : 7
+
+//task3 : multiplication of two numbers
+
+ //  output : multiplication of 4 and 5 : 20
+
+//task4 : division of two numbers
+
+ //  output : division of 24 by 6 : 4
+
+//task5 : remainder when one number is divided by other
+ 
+//  output : remainder when 43 is divided by 7 : 1
+
+
+
+//activity 2 : ASSIGNMENT OPERATOR
+
+
+//task6 : addition assignment operator (+=)
+
+ //  output : addition assignment operator x1 += x2 (x1 = x1 + x2) : 11
+
+//task 7 : subtraction assignment operator (-=)
+
+ //  output : subtraction assignment operator y1 -= y2 (y1 = y1 - y2) : 7
+
+
+
+//activity 3 : COMPARISION OPERATOR
+
+//task 8 : less than and greater than operator
+
+ // output : less than operator 8<10 : true
+console.log("greater then operator 10>8 :",d1>d2); 
+// output : greater then operator 10>8 :false
+
+//task 9 : less than or equal to and greater than or equal to operator
+let e1 = 5;
+let e2 = 6;
+let e3 = 6;
+console.log("greater than equal to operator 5>=6 :",e1>=e2);
+ // output : greater than equal to operator 5>=6 :false
+console.log("less then equal to operator 6<=6: ",e2<=e3); 
+// output : less then equal to operator 6<=6: true
+
+//task 10 : equal to operator
+
+ // output : comparision operator for f1 == f2 : true
+// output : comparision operator for f1===f2 : true
+
+
+
+//activity 4 : LOGICAL OPERATOR
+
+//task11 : AND operator
+
+// output : result of comparision operator and logical operator(and) for 10<6 and 3<5 is : false
+
+//task12 : OR operator
+
+ // output :  result of comparision operator and logical operator(or) for 10<6 or 3<5 is : false
+
+//task13 : NOT operator
+
+ //  output : not operator with comparision operator : false
+
+
+
+//acticity 5 : TERNARY OPERATOR
+
+//task 14 : ternary operator
+
+// output : 25 is positive
+
+
+
+//feature 1 : ARITHEMATIC operator
+
+// output : 40+3 : 43
+
+// output : 40-3 : 37
+
+ // output : 40*3 : 120
+
+// output : 40/3 : 13.33
+// output : 40%3 : 1
+
+
+
+//feature 2 : COMPARISION OPERATOR
+
+
+  // output :  result of 20>10 and 20<10 : false
+
+ //  output : result of 20>10 or 20<10 : true
+ 
+ // output :  result of comparision operator : is 20 equal to 10??  true
+
+
+
+//feature 3 : //TERNARY OPERATOR
+
+ // output : -25 is negative
