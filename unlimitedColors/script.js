f1ed096426b8092f573d716dc272024e6b5bd10d
Ridhima-Sapra <ridhimasapra10@gmail.com>
2025-01-27 19:01:10 +0530

    rectified

--- unlimitedColors/script.js
+++ unlimitedColors/script.js
@@ -1,45 +1,37 @@
 // generate Random colours
 // on clicking start color of bg should randommly change and on stop stop
 
-const hex="0123456789"
-const color="#"
+const hex="0123456789ABCDEF"
 const generateRandomColor=function(){
-    const hex="0123456789"
-    const color="#"
+    let color="#"
     for(let i=0;i<6;i++){
-        color+=hex[Math.round(Math.random()*10)]
+    color+=hex[Math.floor(Math.random()*16)];
     }
     return color;
 }
 //global scope needed
-let IntervalId
-const startbtn=document.getElementById("start")
 
-const stopbtn=document.getElementById("stop")
-
-
-const Startchangebg=function(){
-    if(!IntervalId){
-        IntervalId=setInterval(changeBg,2000)
-    }
-   
-    const changeBg=function(){
-        document.body.style.backgroundColor=generateRandomColor()
+const startbtn=document.querySelector("#start")
+const stopbtn=document.querySelector("#stop")
+let TimeoutExecution=null
+const ChangeBg=function(){
+    document.body.style.backgroundColor= generateRandomColor()
+}
+const StartChangeBg=function(){
+    if(!TimeoutExecution){
+        TimeoutExecution=setInterval(ChangeBg,2000)
     }
 }
 const StopChangeBg=function(){
-    clearInterval(IntervalId)
-    IntervalId=null // cleaner code edge case
+    clearInterval( TimeoutExecution)
+     TimeoutExecution=null
 }
-
-
-startbtn.addEventListener('click' , Startchangebg)
-
-
-stopbtn.addEventListener('click', StopChangeBg)
+startbtn.addEventListener('click',StartChangeBg)
+stopbtn.addEventListener('click',StopChangeBg)
 
 // learning
 // setTimeOut mei jo actually funct perform hora vo daalana as change bg in this
-// fir jis ite pe click krne pe vo function hona chaiye usse select KeyboardEventthen uspe eventListener lga k set/clearInterval vaale
+// fir jis item pe click krne pe vo function hona chaiye usse select KeyboardEventthen uspe eventListener lga k set/clearInterval vaale
 // func ka reference daal do
 // clear vaale mei always reference aata setInterval vaale se
+
